5f5f46b04f5b74a4158e03d225a1cc5ab8195286
nuelkaiser <majekodunmiimmanuel9@gmail.com>
2023-11-07 12:54:04 -1000

    watch product card added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Nav from "@/components/shared/nav/Nav";
 import SideNav from "@/components/sideNav/SideNav";
-
+import CategoryCard from "@/components/category-card/CategoryCard";
+import Watch from "@/assets/images/product-watch.png";
 
 const index = () => {
   const [openNotificationModal, setOpenNotificationModal] = useState(false);
@@ -15,18 +16,25 @@ const index = () => {
     setOpenCartModal(!openCartModal);
   };
 
-
   return (
     <div className=" h-full bg-[#606060] md:flex">
+      <SideNav cart={openCartModal} />
 
-      <SideNav cart={openCartModal}/>
+      <div className=" w-full">
+        <Nav
+          notify={openNotificationModal}
+          cart={openCartModal}
+          handleNotify={handleNotificationModal}
+          handleCart={handleCartModal}
+        />
 
-
-      <Nav notify={openNotificationModal}
-        cart={openCartModal}
-        handleNotify={handleNotificationModal}
-        handleCart={handleCartModal}
-      />
+        <div className=" hidden lg:flex gap-5 px-8">
+          <CategoryCard productName="iWatches" productImg={Watch} />
+          <CategoryCard productName="Chargers" productImg={Watch} />
+          <CategoryCard productName="Screen Guards" productImg={Watch} />
+          <CategoryCard productName="iWatches" productImg={Watch} />
+        </div>
+      </div>
     </div>
   );
 };
